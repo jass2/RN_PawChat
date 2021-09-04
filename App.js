87463fe2809b83d24e109281498570e9cd753e28
Jason <jass2@umbc.edu>
2021-09-04 19:44:47 -0400

    Second commit to add android and ios directories for cross-machine development

--- App.js
+++ App.js
@@ -26,6 +26,20 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRECcflbzGvEILeINjZoM7JD5R4wX1S04',
+  authDomain: 'semesterproj-36773.firebaseapp.com',
+  databaseURL: 'https://semesterproj-36773.firebaseio.com',
+  projectId: 'semesterproj-36773',
+  storageBucket: 'semesterproj-36773.appspot.com',
+  messagingSenderId: '926402690323',
+  appId: '1:926402690323:web:bb346f3baeff8c32322ee6',
+  measurementId: 'G-L83WWZY0JG',
+};
+
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -52,6 +66,10 @@ const Section = ({children, title}): Node => {
   );
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -62,30 +80,30 @@ const App: () => Node = () => {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text> Hi Emma H Smith</Text>
+      {/*<ScrollView*/}
+      {/*  contentInsetAdjustmentBehavior="automatic"*/}
+      {/*  style={backgroundStyle}>*/}
+      {/*  <View*/}
+      {/*    style={{*/}
+      {/*      backgroundColor: isDarkMode ? Colors.black : Colors.white,*/}
+      {/*    }}>*/}
+      {/*    <Section title="Step One">*/}
+      {/*      Edit <Text style={styles.highlight}>App.js</Text> to change this*/}
+      {/*      screen and then come back to see your edits.*/}
+      {/*    </Section>*/}
+      {/*    <Section title="See Your Changes">*/}
+      {/*      <ReloadInstructions />*/}
+      {/*    </Section>*/}
+      {/*    <Section title="Debug">*/}
+      {/*      <DebugInstructions />*/}
+      {/*    </Section>*/}
+      {/*    <Section title="Learn More">*/}
+      {/*      Read the docs to discover what to do next:*/}
+      {/*    </Section>*/}
+      {/*    <LearnMoreLinks />*/}
+      {/*  </View>*/}
+      {/*</ScrollView>*/}
     </SafeAreaView>
   );
 };
